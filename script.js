44d1c5d48b9260af5c056f483b8fae7c578618dd
Thibault Nicholl <hello@designedbytibs.com>
2024-10-22 10:41:34 +0200

    Feature: Hide filter and clear when UI is empty

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 const clearButton = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
 
 const addItem = (e) => {
   e.preventDefault();
@@ -18,8 +19,10 @@ const addItem = (e) => {
 
   const button = createButton('remove-item btn-link text-red');
   li.appendChild(button);
-
+  // Add li to the DOM
   itemList.appendChild(li);
+
+  checkUI();
 
   itemInput.value = '';
 };
@@ -42,7 +45,10 @@ const createIcon = (classes) => {
 
 const removeItem = (e) => {
   if (e.target.parentElement.classList.contains('remove-item')) {
-    e.target.parentElement.parentElement.remove();
+    if (confirm('Are you sure?')) {
+      e.target.parentElement.parentElement.remove();
+      checkUI();
+    }
   }
 };
 
@@ -50,9 +56,25 @@ const clearItems = () => {
   while (itemList.firstChild) {
     itemList.removeChild(itemList.firstChild);
   }
+  checkUI();
+};
+
+// Hide Clear and Filter options when UL is empty
+
+const checkUI = () => {
+  const items = itemList.querySelectorAll('li');
+  if (items.length === 0) {
+    clearButton.style.display = 'none';
+    itemFilter.style.display = 'none';
+  } else {
+    clearButton.style.display = 'block';
+    itemFilter.style.display = 'block';
+  }
 };
 
 // Event Listeners
 itemForm.addEventListener('submit', addItem);
 itemList.addEventListener('click', removeItem);
 clearButton.addEventListener('click', clearItems);
+
+checkUI();
